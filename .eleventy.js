66f221d503d3f73ee43a117cd243110e53e035f5
olee0114 <olee0114@gmail.com>
2022-12-16 15:06:14 -0500

    added stylesheet !

---
+++ .eleventy.js
@@ -0,0 +1,6 @@
+module.exports = function(eleventyConfig) {
+  
+  // add assets folder to _site
+  eleventyConfig.addPassthroughCopy("assets");
+
+};
--- index.md
+++ index.md
@@ -1,1 +1,17 @@
+---
+layout: default
+title: it's a page
+---
+
 # it's a homepage ! 
+
+oogie boogie 
+
+
+
+
+
+
+
+
+
